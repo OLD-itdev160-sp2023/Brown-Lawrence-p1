0cc0e483f4c30002c2851ad0b30866cb0c1f1fa8
Lawrence Brown <browl191@gmatc.matc.edu>
2023-04-25 18:22:31 -0500

    Updated Project 1

--- Project 1/js/scripts.js
+++ Project 1/js/scripts.js
@@ -1,12 +1,10 @@
-function() {
     var price = 800,
         studentDiscount = .30,
         sPrice = price - (price * studentDiscount),
         RegPriceEl = document.getElementById('RegPrice'),
         StudentPriceEl = document.getElementById('StudentPrice');
-        RegPriceEl.textContent = "sometext1";
-        StudentPriceEl.textContent = "Sometext2";
+        RegPriceEl.textContent = price.toFixed(2);
+        StudentPriceEl.textContent = sPrice.toFixed(2);
 
     //document.getElementById('RegPrice').textContent = "5 + 6";
     //document.getElementById('StudentPrice').textContent = "somestong";
-};
